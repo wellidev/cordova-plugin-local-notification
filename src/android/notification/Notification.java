7bb0f9c0ba3d04e471fb29f58be4d79061431613
Wellington Rodrigues <wellington.wrs1@hotmail.com>
2024-07-08 16:10:22 -0300

    Update Notification.java

--- src/android/notification/Notification.java
+++ src/android/notification/Notification.java
@@ -305,7 +305,7 @@ public final class Notification {
             Intent intent = new Intent(action);
 
             PendingIntent pi = PendingIntent.getBroadcast(
-                    context, 0, intent, 0 | PendingIntent.FLAG_IMMUTABLE);
+                    context, 0, intent, 0 | PendingIntent.FLAG_MUTABLE);
 
             if (pi != null) {
                 getAlarmMgr().cancel(pi);
